f280dd823b7fe467b430c7c38ccc10a24cfba4e9
Максим  Корнеев <m.korneev@maximaster.ru>
2022-04-06 17:51:45 +0300

    add file

--- index.php
+++ index.php
@@ -1,5 +1,69 @@
-<?php
-echo '1213';
-echo 'Ветка 3';
-echo 'Ветка 3';
-?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<table class="general-table">
+    <tr class="general-table__row">
+        <td class="general-table__column">
+            <table class="title-table">
+                <tr class="title-table__row">
+                    <th class="title-column">
+                        <h1 class="title-column__name-title">Название сайта (организации)</h1>
+                        <h3 class="title-column__description">Описание сайта</h3>
+                    </th>
+                </tr>
+            </table>
+            <table class="description-table">
+                <tr class="description-table__row">
+                    <td class="left-description-column d">
+                        <h2 class="left-description-column__title">Страница</h2>
+                        <!--Начинаем абзац с красной строки-->
+                        <p class="left-description-column__description-paragraph-1">
+                            Здравствуйте уважаемые будущие веб-мастера!
+                            Мне 55 лет и я рад приветствовать Вас на своём сайте.
+                            Этот сайт первый, который я разработал самостоятельно,
+                            а до этого умел только входить в интернет.</p>
+
+                        <p class="left-description-column__description-paragraph-2">Почему я решил его сделать?
+                            За те 3 месяца, пока разбирался в
+                            сайтостроении и создавал этот ресурс обнаружилось,
+                            что авторы руководств по созданию
+                            сайтов считают многие нюансы само собой разумеющимися
+                            и не обращают на них внимание
+                            А мне, учитывая возраст и «опыт», было не просто
+                            понять как раз эти нюансы, они отнимали больше всего
+                            времени.</p>
+                    </td>
+                    <td class="right-description-column">
+                        <h2 class="right-description-column__title">Страница</h2>
+                        <!--Начинаем абзац с красной строки-->
+                        <p class="right-description-column__description-paragraph-1">
+                            Здравствуйте уважаемые будущие веб-мастера!
+                            Мне 55 лет и я рад приветствовать Вас на своём сайте.
+                            Этот сайт первый, который я разработал самостоятельно,
+                            а до этого умел только входить в интернет.</p>
+
+                        <p class="right-description-column__description-paragraph-2">Почему я решил его сделать?
+                            За те 3 месяца, пока разбирался в
+                            сайтостроении и создавал этот ресурс обнаружилось,
+                            что авторы руководств по созданию
+                            сайтов считают многие нюансы само собой разумеющимися
+                            и не обращают на них внимание
+                            А мне, учитывая возраст и «опыт», было не просто
+                            понять как раз эти нюансы, они отнимали больше всего
+                            времени.</p>
+                    </td>
+                </tr>
+            </table>
+        </td>
+    </tr>
+</table>
+</body>
+</html>
