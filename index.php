2e21757a43ba91c4772983730b1b6a533d3daaff
Максим  Корнеев <m.korneev@maximaster.ru>
2022-04-06 18:00:53 +0300

    add file

--- index.php
+++ index.php
@@ -6,6 +6,8 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet/less" type="text/css" href="css/styles.less" />
+    <script src="javascript/less.js" type="text/javascript"></script>
     <title>Document</title>
 </head>
 <body>
